3b1f72651d9ecea00dcccde09b285cc8bb85ffab
ThaisKuminek <thaiseduarda_22@outlook.com>
2019-08-05 21:57:48 -0300

    Sorteio

--- app/src/main/java/br/edu/ifsc/aula02/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/aula02/MainActivity.java
@@ -2,6 +2,11 @@ package br.edu.ifsc.aula02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view) {
+        EditText nt1 = findViewById(R.id.textView4);
+        EditText nt2 = findViewById(R.id.textView3);
+        TextView sort = findViewById(R.id.textView5);
+
+        int n1 = Integer.parseInt(nt1.getText().toString());
+        int n2 = Integer.parseInt(nt2.getText().toString());
+        Random aleatorio = new Random();
+        int valor = aleatorio.nextInt((n2 - n1) + 1) + n1;
+        sort.setText(Integer.toString(valor));
+
     }
 }
